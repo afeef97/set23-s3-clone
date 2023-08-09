ea240c9aeb66e8bb279d4794d3ab9a71161ab520
Afeef <afeef9706@gmail.com>
2023-08-09 11:03:22 +0800

    Used diskStorage to customise filename

--- src/middleware/upload/image.js
+++ src/middleware/upload/image.js
@@ -1,7 +1,24 @@
 import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/upload/images");
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        cb(
+            null,
+            file.fieldname +
+                "-" +
+                uniqueSuffix +
+                path.extname(file.originalname)
+        );
+    },
+});
 
 const uploadImage = multer({
-    dest: "public/upload",
+    storage,
 });
 
 export default uploadImage;
